03c87231ed6e77a4bf553b84b55e79063d09bbc0
전병길 <jeonbg@kakao.com>
2025-12-13 01:01:24 +0900

    feat: EXPO 53

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+
+export default function RootLayout() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
---
+++ mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function IndexPage() {
+  return (
+    <View>
+      <Text>안녕하세요</Text>
+    </View>
+  );
+}
